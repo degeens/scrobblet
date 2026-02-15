e3bf0bc27e2bd3cd6e2ab03080fe539fb470dae5
Stijn Geens <33414262+degeens@users.noreply.github.com>
2026-02-15 22:09:21 +0100

    Remove port from request logs

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"log/slog"
+	"net"
 	"net/http"
 )
 
 func logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var (
-			ip     = r.RemoteAddr
-			proto  = r.Proto
-			method = r.Method
-			uri    = r.URL.RequestURI()
-		)
+		method := r.Method
+		uri := r.URL.RequestURI()
+		proto := r.Proto
 
-		slog.Info("Received request", "ip", ip, "proto", proto, "method", method, "uri", uri)
+		ip := r.RemoteAddr
+		if host, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+			ip = host
+		}
+
+		slog.Info("Received request", "method", method, "uri", uri, "proto", proto, "ip", ip)
 
 		next.ServeHTTP(w, r)
 	})
